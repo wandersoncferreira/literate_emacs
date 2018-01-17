732e580c00597f0e6e3bfb8b478e4457a9ef500d
Wanderson Ferreira <iagwanderson@gmail.com>
2018-01-17 01:19:00 -0200

    ivy, counsel

--- emacs/init.el
+++ emacs/init.el
@@ -11,3 +11,31 @@
   (package-install 'use-package))
 
 (use-package diminish :ensure t :defer t)
+
+
+;;; completions
+(use-package ivy
+  :ensure t
+  :diminish ivy-mode
+  :init
+  (setq ivy-height 8)
+  (setq ivy-use-virtual-buffers t
+	    ivy-current-matching nil
+	    ivy-minibuffer-faces nil
+	    ivy-fixed-height-minibuffer t
+	    ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
+  :bind (:map ivy-minibuffer-map
+		  ("C-j" . ivy-immediate-done)
+		  ("RET" . ivy-alt-done))
+  :config
+  (add-hook 'after-init-hook 'ivy-mode))
+
+
+(use-package counsel
+  :ensure t
+  :config
+  (use-package smex :ensure t :config (smex-initialize))
+  (add-hook 'after-init-hook 'counsel-mode)
+  :bind
+  (("M-x" . counsel-M-x)
+   ("C-x C-m" . counsel-M-x)))
