c0c72ad7245ab6b0c73587326deb108de8a8d26d
Wanderson Ferreira <iagwanderson@gmail.com>
2018-01-17 01:18:04 -0200

    init packages

---
+++ emacs/init.el
@@ -0,0 +1,13 @@
+;;; Here be dragons!!
+
+
+;;; packages
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/"))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package diminish :ensure t :defer t)
