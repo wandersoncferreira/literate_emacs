617ed82399a54eff08a09591a149802978fae574
Wanderson Ferreira <iagwanderson@gmail.com>
2018-01-17 01:21:46 -0200

    user interface

--- emacs/init.el
+++ emacs/init.el
@@ -12,6 +12,26 @@
 
 (use-package diminish :ensure t :defer t)
 
+;;; user interface
+(use-package base16-theme :ensure t)
+(load-theme 'base16-google-light t)
+
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+
+(when (display-graphic-p)
+  (setq frame-title-format
+	    '((:eval (if (buffer-file-name)
+			 (abbreviate-file-name (buffer-file-name))
+		       "%b")))))
+
+(when (display-graphic-p)
+  (setq-default cursor-type 'bar))
+
+(column-number-mode +1)
+(display-time-mode +1)
+
 
 ;;; completions
 (use-package ivy
@@ -39,3 +59,18 @@
   :bind
   (("M-x" . counsel-M-x)
    ("C-x C-m" . counsel-M-x)))
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (smex counsel ivy diminish use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
