7bb0b1c7916ca53049c116b6bc361ecad1d6e804
Wanderson Ferreira <iagwanderson@gmail.com>
2018-01-18 07:08:48 -0200

    initial python mode config

--- emacs/init.el
+++ emacs/init.el
@@ -16,19 +16,23 @@
 (use-package base16-theme :ensure t)
 (load-theme 'base16-google-light t)
 
+;; removing distractive ui
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 
+;; improve the frame title in Emacs to show the path to file visited
 (when (display-graphic-p)
   (setq frame-title-format
 	    '((:eval (if (buffer-file-name)
 			 (abbreviate-file-name (buffer-file-name))
 		       "%b")))))
 
+;; cursor like a thin bar. Idk why I got used to it.
 (when (display-graphic-p)
   (setq-default cursor-type 'bar))
 
+;; more info into the modeline
 (column-number-mode +1)
 (display-time-mode +1)
 
@@ -49,14 +53,25 @@
   (("M-x" . counsel-M-x)
    ("C-x C-m" . counsel-M-x)))
 
+;;; Python mode
+(use-package python
+  :mode ("\\.py$\\'" . python-mode)
+  :init
+  (setq python-shell-completion-native-enable nil))
 
+(use-package elpy
+  :ensure t
+  :after python
+  :config
+  (elpy-enable))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (smex counsel ivy diminish use-package))))
+ '(delete-selection-mode nil)
+ '(package-selected-packages (quote (elpy smex counsel ivy diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
