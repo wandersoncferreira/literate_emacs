60ee0cce66d3640fbf8fb199379fc96889244039
Wanderson Ferreira <iagwanderson@gmail.com>
2018-12-09 10:27:19 -0200

    adding headers to the smex setup file

--- settings/setup-smex.el
+++ settings/setup-smex.el
@@ -1,6 +1,14 @@
+;;; setup-smex --- SMEX
+;;; Commentary:
+
+;; Better M-x for your life
+
+;;; Code:
+
 (bk/install-maybe-require 'smex)
 
 (setq smex-prompt-string "Here be dragons => ")
 (smex-initialize)
 
 (provide 'setup-smex)
+;;; setup-smex.el ends here
