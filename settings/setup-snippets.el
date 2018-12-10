c39275fb1b1d1325da814c2e25a288a7153ab389
Wanderson Ferreira <iagwanderson@gmail.com>
2018-12-10 01:43:55 -0200

    IMPROV: add functions to move inside snippets fields

--- settings/setup-snippets.el
+++ settings/setup-snippets.el
@@ -7,14 +7,35 @@
 
 
 (bk/install-maybe-require 'yasnippet)
-(bk/install-maybe-require 'yasnippet-snippets)
-(bk/install-maybe-require 'clojure-snippets)
-(bk/install-maybe-require 'react-snippets)
 
 (yas-global-mode +1)
 (diminish 'yas-minor-mode)
 
-;; jump to end of snippet definition
+;; include snippets
+(bk/install-maybe-require 'yasnippet-snippets)
+(bk/install-maybe-require 'clojure-snippets)
+(bk/install-maybe-require 'react-snippets)
+
+(defun yas/goto-end-of-active-field ()
+  "Go to the end of the active field."
+  (interactive)
+  (let* ((snippet (car (yas--snippets-at-point)))
+         (position (yas--field-end (yas--snippet-active-field snippet))))
+    (if (= (point) position)
+        (move-end-of-line 1)
+      (goto-char position))))
+
+(defun yas/goto-start-of-active-field ()
+  "Go to the beginning of the active field."
+  (interactive)
+  (let* ((snippet (car (yas--snippets-at-point)))
+         (position (yas--field-start (yas--snippet-active-field snippet))))
+    (if (= (point) position)
+        (move-beginning-of-line 1)
+      (goto-char position))))
+
+(define-key yas-keymap (kbd "C-e") 'yas/goto-end-of-active-field)
+(define-key yas-keymap (kbd "C-a") 'yas/goto-start-of-active-field)
 (define-key yas-keymap (kbd "<return>") 'yas-exit-all-snippets)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
 (define-key yas-minor-mode-map (kbd "<tab>") nil)
