f9d444864d47dc9ee32d962998648a5631b52322
Wanderson Ferreira <iagwanderson@gmail.com>
2019-01-03 08:43:46 -0200

    IMPROV: clojure editing settings
    There was an error in the configuration that prevent cljs buffers to
    work properly. Also added the kibit package to help with code quality

--- settings/setup-clojure.el
+++ settings/setup-clojure.el
@@ -8,6 +8,12 @@
 (bk/install-maybe 'clojure-mode-extra-font-locking)
 (bk/install-maybe 'clj-refactor)
 (bk/install-maybe 'cider)
+(bk/install-maybe 'kibit-helper)
+
+(require 'clojure-mode)
+(require 'clj-refactor)
+(require 'cider)
+(require 'kibit-helper)
 
 (defun my-clojure-mode-hook ()
   "Activate the refactor library."
@@ -25,27 +31,29 @@
   "Evaluate the figwheel code inside a regular clojure repl."
   (interactive)
   (save-some-buffers)
-  (with-current-buffer (cider-current-repl)
+  (with-current-buffer (cider-current-repl-buffer)
     (goto-char (point-max))
     (insert "(require 'figwheel-sidecar.repl-api)
 	     (figwheel-sidecar.repl-api/start-figwheel!)
 	     (figwheel-sidecar.repl-api/cljs-repl)")
     (cider-repl-return)))
 
+(add-to-list 'auto-mode-alist '("\\.cljs\\'" . clojurescript-mode))
 
-(with-eval-after-load 'clojure-mode
-  (define-key clojure-mode-map [remap paredit-forward] 'clojure-forward-logical-sexp)
-  (define-key clojure-mode-map [remap paredit-backward] 'clojure-backward-logical-sexp)
-  (setq cljr-favor-prefix-notation nil
-	cljr-favor-private-functions nil)
-  (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
-  (add-hook 'clojure-mode-hook 'cider-mode))
+(define-key clojure-mode-map [remap paredit-forward] 'clojure-forward-logical-sexp)
+(define-key clojure-mode-map [remap paredit-backward] 'clojure-backward-logical-sexp)
 
-(with-eval-after-load 'cider
-  (setq cider-repl-result-prefix ";; => "
-	cider-prompt-for-symbol nil
-	nrepl-hide-special-buffers nil
-	cljr-inject-dependencies-at-jack-in nil))
+(setq cljr-favor-prefix-notation nil
+      cljr-favor-private-functions nil)
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+(add-hook 'clojure-mode-hook 'cider-mode)
+
+
+(setq cider-repl-result-prefix ";; => "
+      cider-prompt-for-symbol nil
+      nrepl-hide-special-buffers nil)
+
 
 (provide 'setup-clojure)
 ;;; setup-clojure.el ends here
