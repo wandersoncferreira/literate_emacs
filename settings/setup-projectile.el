94bfa7daef4488217f79ee9ae884ad93ce5963d3
Wanderson Ferreira <iagwanderson@gmail.com>
2019-01-23 19:53:26 -0200

    change the prefix name of the projectile mode-line

--- settings/setup-projectile.el
+++ settings/setup-projectile.el
@@ -8,15 +8,13 @@
 (bk/install-maybe 'projectile)
 (require 'projectile)
 
+(setq-default projectile-mode-line-prefix " Proj")
+(setq projectile-mode-line-function '(lambda () (format " Proj[%s]" (projectile-project-name))))
+
 (setq projectile-completion-system 'ido
       projectile-switch-project-action 'projectile-find-file
       projectile-project-search-path '("~/captalys" "~/personal"))
 
-(setq projectile-mode-line
-      '(:eval (if (projectile-project-p)
-		  (format " P[%s]"
-			  (projectile-project-name))
-		"")))
 
 (eval-after-load 'projectile
   '(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
