2540c067f15658a8acb27bf9890e78f8ff3bca87
Wanderson Ferreira <iagwanderson@gmail.com>
2019-01-23 19:54:01 -0200

    use package to manage the hideshow package loading

--- settings/setup-misc.el
+++ settings/setup-misc.el
@@ -31,7 +31,11 @@
 (diminish 'whitespace-mode)
 
 (add-hook 'prog-mode-hook 'prettify-symbols-mode)
-(add-hook 'prog-mode-hook 'hs-minor-mode)
+
+(use-package hideshow
+  :diminish 'hs-minor-mode
+  :config
+  (add-hook 'prog-mode-hook 'hs-minor-mode))
 
 (global-whitespace-cleanup-mode +1)
 (diminish 'whitespace-cleanup-mode)
