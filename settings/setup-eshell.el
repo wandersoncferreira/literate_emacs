7ec8613ecb547c56e29923307b3710966b84e484
Wanderson Ferreira <iagwanderson@gmail.com>
2019-03-03 22:48:46 -0300

    remove shell-command package

--- settings/setup-eshell.el
+++ settings/setup-eshell.el
@@ -5,11 +5,7 @@
 
 ;;; Code:
 
-(bk/install-maybe 'shell-command)
-
 (require 'eshell)
-(require 'shell-command)
-(require 'bash-completion)
 
 (defun eshell-clear-buffer ()
   "Clear terminal."
@@ -30,27 +26,6 @@
 	    (eshell/alias "d" "dried $1")))
 
 (add-hook 'eshell-mode-hook #'abbrev-mode)
-
-(autoload 'bash-completion-dynamic-complete
-  "bash-completion"
-  "BASH completion hook")
-
-(add-hook 'shell-dynamic-complete-functions
-	  'bash-completion-dynamic-complete)
-(add-hook 'shell-command-completion-function
-	  'bash-completion-dynamic-complete)
-
-(shell-command-completion-mode)
-
-(defun comint-delchar-or-eof-or-kill-buffer (arg)
-  "To kill buffer if process if dead, ARG."
-  (interactive "p")
-  (if (null (get-buffer-process (current-buffer)))
-      (kill-buffer)
-    (comint-delchar-or-maybe-eof arg)))
-(add-hook 'shell-mode-hook
-	  (lambda ()
-	    (define-key shell-mode-map (kbd "C-d") 'comint-delchar-or-eof-or-kill-buffer)))
 
 (provide 'setup-eshell)
 ;;; setup-eshell.el ends here
