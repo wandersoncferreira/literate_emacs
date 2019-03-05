ed7048b82b8b3046ffb8ddc57fe26d476c5c9256
Wanderson Ferreira <iagwanderson@gmail.com>
2019-03-05 11:43:55 -0300

    I needed to install several linters to work with flycheck

--- settings/setup-flycheck.el
+++ settings/setup-flycheck.el
@@ -10,9 +10,19 @@
 
 (flycheck-add-mode 'javascript-eslint 'web-mode)
 (flycheck-add-mode 'javascript-eslint 'rjsx-mode)
-
+(setq flycheck-display-errors-delay .3)
 (setq flycheck-check-syntax-automatically '(save
 					    mode-enabled))
+
+;;; packages to install here -- linters!
+;; npm install jsonlint -g  # json
+;; brew install hadolint  # dockerfile
+;; npm install -g eslint  # javascript
+;;; you should then setup a configuration file: eslint --init
+;; npm install -g jshint  # javascript
+;; npm install -g markdownlint-cli  # markdown
+;; gem install mdl  # markdown
+;; npm install -g js-yaml
 
 (provide 'setup-flycheck)
 ;;; setup-flycheck.el ends here
