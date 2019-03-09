482fb4b059516aa318f9e8c67c14331af313852e
Wanderson Ferreira <iagwanderson@gmail.com>
2019-03-09 20:12:03 -0300

    highlight the diff in the fringe

--- settings/setup-git.el
+++ settings/setup-git.el
@@ -8,6 +8,7 @@
 (bk/install-maybe 'browse-at-remote)
 (bk/install-maybe 'magit-todos)
 (bk/install-maybe 'gitconfig-mode)
+(bk/install-maybe 'diff-hl)
 
 (require 'magit)
 (require 'magit-todos)
@@ -26,6 +27,14 @@
 
 (add-to-list 'auto-mode-alist '("\\.gitconfig$" . gitconfig-mode))
 (add-to-list 'auto-mode-alist '("\\.gitignore$" . gitignore-mode))
+
+;; diff
+(add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+(add-hook 'after-init-hook 'global-diff-hl-mode)
+
+(with-eval-after-load 'diff-hl
+  (define-key diff-hl-mode-map (kbd "<left-fringe> <mouse-1>")
+    'diff-hl-diff-goto-hunk))
 
 (provide 'setup-git)
 ;;; setup-git.el ends here
