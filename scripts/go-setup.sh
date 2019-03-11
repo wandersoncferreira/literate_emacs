c491a9119c009aebac079abaf9f5153717d5c38b
Wanderson Ferreira <iagwanderson@gmail.com>
2019-03-11 03:14:09 -0300

    some shell scripts that I need to call from emacs later

---
+++ scripts/go-setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo 'install go complete daemon'
+go get -u github.com/nsf/gocode
+
+echo 'install go definitions'
+go get -u github.com/rogpeppe/godef/...
+
+echo 'install go imports'
+go get -u golang.org/x/tools/cmd/goimports
+
+echo 'install go tools'
+go get -u golang.org/x/tools/cmd/...
+go get -u golang.org/x/tools/cmd/guru
+
+echo 'install go check'
+go get -u github.com/dougm/goflymake
---
+++ scripts/python-setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+python -m pip install jedi
+python -m pip install autopep8
+python -m pip install yapf
+python -m pip install black
+python -m pip install ipython
+python -m pip install rope
