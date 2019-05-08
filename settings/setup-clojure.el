07f40f6fe8126a0a781ed7db472d567b9851f0ef
Wanderson Ferreira <iagwanderson@gmail.com>
2019-05-08 01:04:55 -0300

    setup on clojure mode

--- settings/setup-clojure.el
+++ settings/setup-clojure.el
@@ -7,24 +7,39 @@
 (bk/install-maybe 'clojure-mode)
 (bk/install-maybe 'clojure-mode-extra-font-locking)
 (bk/install-maybe 'clj-refactor)
-(bk/install-maybe 'cider)
 
 (require 'clojure-mode)
 (require 'clj-refactor)
-(require 'cider)
+
+(use-package cider
+  :ensure t
+  :init
+  (setq cider-repl-history-file "~/.emacs.d/cider-history"
+        cider-repl-use-pretty-printing t
+        cider-repl-use-clojure-font-lock t
+        cider-repl-result-prefix ";; => "
+        cider-repl-wrap-history t
+        cider-repl-history-size 3000)
+  :config
+  (add-hook 'cider-mode-hook #'eldoc-mode))
+
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+
+(use-package flycheck-clojure
+  :ensure t
+  :after flycheck
+  :config
+  (use-package let-alist :ensure t)
+  (flycheck-clojure-setup))
 
 (defun my-clojure-mode-hook ()
   "Activate the refactor library."
   (clj-refactor-mode +1)
   (cljr-add-keybindings-with-prefix "C-c C-m"))
 
-(defun start-cider-repl-with-profile ()
-  (interactive)
-  (letrec ((profile (read-string "Enter profile name: "))
-           (lein-parms (concat "with-profile +" profile " repl :headless")))
-    (message "lein-params set to: %s" lein-parms)
-    (set-variable 'cider-lein-parameters lein-parms)
-    (cider-jack-in)))
 
 (defun cider-figwheel-repl ()
   "Evaluate the figwheel code inside a regular clojure repl."
