aa492f3f7d0f4db1ce76662bc4241600f2b12aa7
Wanderson Ferreira <iagwanderson@gmail.com>
2019-07-21 17:00:27 -0300

    removing hardcode from projectile search folder path

--- settings/setup-projectile.el
+++ settings/setup-projectile.el
@@ -12,11 +12,9 @@
   (setq projectile-mode-line-function
         '(lambda () (format " Proj[%s]" (projectile-project-name)))
         projectile-completion-system 'ido
-        projectile-switch-project-action 'projectile-find-file
-        projectile-project-search-path '("~/platform"))
+        projectile-switch-project-action 'projectile-find-file)
   :config
   (projectile-mode +1))
-
 
 (provide 'setup-projectile)
 ;;; setup-projectile.el ends here
