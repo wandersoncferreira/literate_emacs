6040a71b13dd535ef36139b388b19075e29dba7a
Wanderson Ferreira <iagwanderson@gmail.com>
2020-03-25 21:28:26 -0300

    more customizations

--- custom.el
+++ custom.el
@@ -31,7 +31,15 @@
       (slot . 0)))))
  '(package-selected-packages
    (quote
-    (flyspell-correct auctex yasnippet-snippets py-autopep8 elpy flycheck-clj-kondo aggressive-indent clj-refactor cider-eval-sexp-fu cider clojure-mode flycheck hl-todo paredit ws-butler slack elfeed magit git-timemachine yaml-mode which-key use-package smex restart-emacs projectile plantuml-mode paren-face org-gcal multiple-cursors move-dup modus-operandi-theme json-mode ido-completing-read+ hydra highlight-numbers expand-region exec-path-from-shell diminish crm-custom cheatsheet ace-window)))
+    (change-inner markdown-mode windresize restclient tomatinho flyspell-correct auctex yasnippet-snippets py-autopep8 elpy flycheck-clj-kondo aggressive-indent clj-refactor cider-eval-sexp-fu cider clojure-mode flycheck hl-todo paredit ws-butler slack elfeed magit git-timemachine yaml-mode which-key use-package smex restart-emacs projectile plantuml-mode paren-face org-gcal multiple-cursors move-dup modus-operandi-theme json-mode ido-completing-read+ hydra highlight-numbers expand-region exec-path-from-shell diminish crm-custom cheatsheet ace-window)))
+ '(safe-local-variable-values
+   (quote
+    ((cider-docker-translations
+      ("/app/src" . "/home/wand/platform/register/src"))
+     (cider-docker-translations
+      ("/app/src" . "/home/wand/platform/banker/src")
+      ("/app/test" . "/home/wand/platform/banker/test"))
+     (TeX-command-extra-options . "-shell-escape"))))
  '(which-key-prefix-prefix "+")
  '(which-key-separator " "))
 (custom-set-faces
