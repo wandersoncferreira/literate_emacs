0accf566992f2730b97f0526115c2e2711011fe7
Wanderson Ferreira <iagwanderson@gmail.com>
2020-03-26 07:44:49 -0300

    very focused and clean setup

---
+++ init.el
@@ -0,0 +1,87 @@
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (file-exists-p "~/.emacs.d/elpa/archives/melpa")
+  (package-refresh-contents))
+
+(setq my-external-packages '(cider
+			     paredit
+			     clojure-mode
+			     magit
+			     change-inner
+			     smex
+			     projectile))
+
+(dolist (pkg my-external-packages)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
+
+(global-set-key "\C-x2" (lambda ()
+			  (interactive)
+			  (split-window-horizontally)
+			  (other-window 1)))
+
+(global-set-key "\C-x3" (lambda ()
+			  (interactive)
+			  (split-window-vertically)
+			  (other-window 1)))
+(require 'smex)
+(smex-initialize)
+
+(global-set-key (kbd "C-x C-m") 'smex)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-i") 'change-inner)
+(global-set-key (kbd "M-o") 'change-outer)
+(global-set-key (kbd "C-c e") 'eshell)
+(global-set-key (kbd "C-c g s") 'magit-status)
+(global-set-key (kbd "C-c C-k") 'eval-buffer)
+(global-set-key (kbd "C-x C-j") 'dired-jump)
+
+;; disable modes
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; enable modes
+(electric-pair-mode)
+(show-paren-mode)
+(projectile-mode)
+(delete-selection-mode)
+(pending-delete-mode)
+(ido-mode)
+(ido-everywhere)
+
+(add-hook 'lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'clojure-mode-hook #'enable-paredit-mode)
+
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+(setq tab-always-indent 'complete)
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups"))))
+
+(add-hook 'eshell-mode-hook
+	  (lambda ()
+	    (eshell/alias "e" "find-file $1")
+	    (eshell/alias "ee" "find-file-other-window $1")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((cider-docker-translations
+      ("/app/src" . "/home/wand/platform/banker/src")
+      ("/app/test" . "/home/wand/platform/banker/test"))
+     (cider-docker-translations
+      ("/app/src" . "/home/wand/platform/register/src"))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
