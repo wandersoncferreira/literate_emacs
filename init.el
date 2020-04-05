26fb4990ba840583450a8042cd8209ae5199c5e5
Wanderson Ferreira <iagwanderson@gmail.com>
2020-04-05 07:27:52 -0300

    startup time matters and gc is too aggressive in emacs

--- init.el
+++ init.el
@@ -7,6 +7,18 @@
 
 ;;; Code:
 
+;; startup time matters. This bit will improve garbage collector of Emacs during its loading process.
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024))
+      (bk--file-name-handler-alist file-name-handler-alist))
+  (setq gc-cons-threshold init-gc-cons-threshold
+        gc-cons-percentage 0.6
+        file-name-handler-alist nil)
+  (add-hook 'emacs-startup-hook
+            (lambda ()
+              (setq gc-cons-threshold normal-gc-cons-threshold
+                    gc-cons-percentage 0.1
+                    file-name-handler-alist bk--file-name-handler-alist))))
 (require 'org)
 (setq vc-follow-symlinks t)
 (org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
