c6f9ebd6934cfde57d541469ea07c5d3e04996aa
Wanderson Ferreira <iagwanderson@gmail.com>
2020-04-05 07:47:40 -0300

    disable theme before loading the new one.

--- custom.el
+++ custom.el
@@ -1,3 +1,7 @@
+;;; custom --- Custom variables and alike
+;;; Commentary:
+;;; Code:
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -5,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (delight toc-org yasnippet-snippets rg modus-vivendi-theme modus-operandi-theme elfeed ace-window windresize clojure-mode-extra-font-locking avy which-key hc-zenburn-theme anti-zenburn-theme zenburn-theme git-timemachine plantuml-mode clj-refactor highlight-indentation smart-shift night-owl-theme minimal-theme warm-night-theme monochrome-theme humanoid-themes nord-theme mlso-theme gotham-theme eshell-bookmark docker docker-compose-mode dockerfile-mode fantom-theme markdown-mode smartparens flycheck-clj-kondo flycheck multiple-cursors restclient color-theme-sanityinc-tomorrow json-mode tomatinho smex projectile paredit magit cider change-inner)))
+    (esup delight toc-org yasnippet-snippets rg modus-vivendi-theme modus-operandi-theme elfeed ace-window windresize clojure-mode-extra-font-locking avy which-key hc-zenburn-theme anti-zenburn-theme zenburn-theme git-timemachine plantuml-mode clj-refactor highlight-indentation smart-shift night-owl-theme minimal-theme warm-night-theme monochrome-theme humanoid-themes nord-theme mlso-theme gotham-theme eshell-bookmark docker docker-compose-mode dockerfile-mode fantom-theme markdown-mode smartparens flycheck-clj-kondo flycheck multiple-cursors restclient color-theme-sanityinc-tomorrow json-mode tomatinho smex projectile paredit magit cider change-inner)))
  '(safe-local-variable-values
    (quote
     ((cider-docker-translations
@@ -29,3 +33,6 @@
       ("/app/test" . "/home/wand/platform/banker/test"))
      (cider-docker-translations
       ("/app/src" . "/home/wand/platform/register/src"))))))
+
+(provide 'custom)
+;;; custom.el ends here
