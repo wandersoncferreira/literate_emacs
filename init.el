017bee2737f54ffb2542e75eb6816aeb8681fe70
Wanderson Ferreira <iagwanderson@gmail.com>
2020-04-11 08:35:17 -0300

    fix gc and add more docs

--- init.el
+++ init.el
@@ -7,17 +7,6 @@
 
 ;;; Code:
 
-;; startup time matters. This bit will improve garbage collector of Emacs during its loading process.
-(let ((init-gc-cons-threshold (* 128 1024 1024))
-      (bk--file-name-handler-alist file-name-handler-alist))
-  (setq gc-cons-threshold init-gc-cons-threshold
-        gc-cons-percentage 0.6
-        file-name-handler-alist nil)
-  (add-hook 'emacs-startup-hook
-            (lambda ()
-              (setq gc-cons-threshold 800000
-                    gc-cons-percentage 0.1
-                    file-name-handler-alist bk--file-name-handler-alist))))
 (require 'org)
 (setq vc-follow-symlinks t)
 (org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
