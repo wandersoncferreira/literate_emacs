c946fc4536d85941ac7a55d512c847a18b268879
Wanderson Ferreira <iagwanderson@gmail.com>
2020-05-01 17:42:08 -0300

    sync setup after several interations

--- init.el
+++ init.el
@@ -8,9 +8,19 @@
 
 ;;; Code:
 
+(defconst emacs-start-time (current-time))
+
 (require 'org)
 (setq vc-follow-symlinks t)
 (org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
+
+(add-hook 'after-init-hook
+          `(lambda ()
+             (let ((elapsed
+                    (float-time
+                     (time-subtract (current-time) emacs-start-time))))
+               (message "Loading %s...done (%.3fs) [after-init]"
+                        ,load-file-name elapsed))) t)
 
 (provide 'init)
 
